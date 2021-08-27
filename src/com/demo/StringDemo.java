69267a86c80f6188c7a5c69f12c1c7a0ce346d3d
BalajiKole <tqbalaji@gmail.com>
2021-08-27 16:25:35 +0530

    commit

--- src/com/demo/StringDemo.java
+++ src/com/demo/StringDemo.java
@@ -1,12 +1,9 @@
 package com.demo;
 
 public class StringDemo {
-public static void main(String[] args) {
-	String str="TQ";
-	String str1="Hello";
-	String str2="Tqpp";
-	int num=100;
-	System.out.println(str==str1);
-	System.out.println(str==str2);
-}
+	public static void main(String[] args) {
+		String str = "TQ";
+		String str1 = "Hello";
+		System.out.println(str == str1);
+	}
 }
