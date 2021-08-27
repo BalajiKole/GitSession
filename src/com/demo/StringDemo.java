6e77926b53d5d008293c7436389ed3508f4b4db3
BalajiKole <66683134+BalajiKole@users.noreply.github.com>
2021-08-27 16:02:53 +0530

    in StringDemo class num and string added

--- src/com/demo/StringDemo.java
+++ src/com/demo/StringDemo.java
@@ -4,6 +4,9 @@ public class StringDemo {
 public static void main(String[] args) {
 	String str="TQ";
 	String str1="Hello";
+	String str2="Tqpp";
+	int num=100;
 	System.out.println(str==str1);
+	System.out.println(str==str2);
 }
 }
